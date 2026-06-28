1311fc6fac70939f234586874be601b8eba37dae
solankydevyansh-ship-it <solankydevyansh@gmail.com>
2026-06-28 10:15:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const button=document.getElementById("openBtn");
+
+const home=document.getElementById("home");
+
+const letter=document.getElementById("letter");
+
+button.addEventListener("click",()=>{
+
+home.style.display="none";
+
+letter.style.display="block";
+
+});
